789235044cae90e1a1fb12e909f4e6eeb49f2488
leecurrent04 <leecurrent04@gmail.com>
2025-07-25 23:07:46 +0900

    refactor : BMI323
    - BMI323 함수 이름 및 불필요한 코드 리펙토링
    - SPI 코드 리펙토링

--- Plane/Core/Src/FC_Basic/SPI.c
+++ Plane/Core/Src/FC_Basic/SPI.c
@@ -13,6 +13,15 @@
 
 
 /* Functions -----------------------------------------------------------------*/
+/*
+ * @brief SPI 활성화
+ * @detail
+ * 		SPI가 활성화 되어있지 않으면, 활성화함.
+ * @param
+ * 		SPI_TypeDef* spi : SPI 주소 (ex. SPI1)
+ * @retval
+ * 		None
+ */
 void SPI_Enable(SPI_TypeDef* spi){
 	if(!LL_SPI_IsEnabled(spi)){
 		LL_SPI_Enable(spi);
@@ -21,7 +30,18 @@ void SPI_Enable(SPI_TypeDef* spi){
 }
 
 
-unsigned char SPI_SendByte(SPI_TypeDef* spi, unsigned char data)
+/*
+ * @brief SPI 1Byte 전송
+ * @detail
+ * 		1Byte를 전송하고 리턴된 값을 반환함.
+ * 		4-wire SPI에서 동작.
+ * @param
+ * 		SPI_TypeDef* spi : SPI 주소 (ex. SPI1)
+ *		uint8_t data : 송신할 데이터
+ * @retval
+ * 		uint8_t : 수신 데이터
+ */
+uint8_t SPI_SendByte(SPI_TypeDef* spi, uint8_t data)
 {
 	while(LL_SPI_IsActiveFlag_TXE(spi)==RESET);
 	LL_SPI_TransmitData8(spi, data);
